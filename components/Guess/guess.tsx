3211d8dc9c00e7509f471e64ff1d68549f5e305a
Sam Fish <samuel.fish@live.com>
2023-10-26 18:43:59 -0700

    checking in state

---
+++ components/Guess/guess.tsx
@@ -0,0 +1,12 @@
+export default function Guess() {
+  return (
+    <div
+      style={{
+        backgroundColor: "red",
+        margin: "5px",
+        width: "100px",
+        height: "100px",
+      }}
+    ></div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
+import Guess from "../components/Guess/guess";
+
+const NUM_OF_GUESSES_ALLOWED = 5;
 
 export default function Home() {
   const [guess, setGuess] = useState("");
+
+  const guessGrid: number[][] = new Array(6)
+    .fill(0)
+    .map(() => new Array(5).fill({ call: "1" }));
+    const array = [[null,null,null],[null,null,null]]
+
+    return (
+    <div> {array.map(x=> x.map(y=> <div>{....}</div> ))}</div>
+    )
   return (
     <div className={styles.container}>
+      {guessGrid.map(value => {
+          return (<>
+            <p>{value}</p>
+            <Guess />
+          </>);}
+        ))};
+
       <form>
         <p>worlde:</p>
         <input
